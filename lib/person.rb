4efa688615a119ef445aeea389bcea4a0c94d092
yfern328 <yfernan2@u.rochester.edu>
2017-06-09 09:36:09 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -55,11 +55,9 @@ class Person
   end
 
   def call_friend(friend)
-
     self.happiness=(self.happiness + 3)
     friend.happiness=(friend.happiness + 3)
     "Hi #{friend.name}! It's #{self.name}. How are you?"
-
   end
 
   def start_conversation(person, topic)
